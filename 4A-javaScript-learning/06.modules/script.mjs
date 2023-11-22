211858fb279a11ca6030e3436724646568fb258f
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2023-11-22 14:54:55 +0530

    Delete 4A-javaScript-learning directory

--- 4A-javaScript-learning/06.modules/script.mjs
+++
@@ -1,4 +0,0 @@
-import data,{b,c} from "./script2.mjs";
-console.log(data);
-console.log(b);
-console.log(c);
--- 4A-javaScript-learning/06.modules/script2.mjs
+++
@@ -1,6 +0,0 @@
-let a="sudhir";
-let b="hrishabh";
-let c= "nandan kumar";
-
-export default a;
-export {b,c};
